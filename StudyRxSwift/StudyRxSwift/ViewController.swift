52491bb043b326c3ca75d10da4b04874939c7ec4
kyungpyoda <44656036+kyungpyoda@users.noreply.github.com>
2021-04-01 14:02:37 +0900

    [chore] Add, apply pod 'RxCocoa'

--- StudyRxSwift/StudyRxSwift/ViewController.swift
+++ StudyRxSwift/StudyRxSwift/ViewController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let observable = Observable.of("Hello", "RxSwift")
+        observable.subscribe(onNext: { print("첫번째: \($0)") })
+        observable.subscribe(onNext: { print("두번째: \($0)") })
     }
 
 
