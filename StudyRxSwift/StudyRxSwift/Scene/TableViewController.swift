6b1b52bf0e2049edce48e007c80b0458cf29bf92
kyungpyoda <44656036+kyungpyoda@users.noreply.github.com>
2021-04-05 14:21:40 +0900

    Implement TableView with RxSwift (step4)
    - SearchBar 추가, 검색 기능 구현

--- StudyRxSwift/StudyRxSwift/Scene/TableViewController.swift
+++ StudyRxSwift/StudyRxSwift/Scene/TableViewController.swift
@@ -20,20 +20,53 @@ class TableViewController: UIViewController {
     
     private let mockAPI = "https://my.api.mockaroo.com/piomock.json?key=5db4e260"
     
-    private var items = PublishSubject<[Item]>()
+    private var items: [Item] = []
+    private let filterItems = PublishSubject<[Item]>()
     
     var disposeBag = DisposeBag()
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUp()
+    }
+    
+    private func setUp() {
+        setUpSearchBar()
+        binding()
+    }
+    
+    private func setUpSearchBar() {
+        searchBar.rx.text
+            .orEmpty
+            .debounce(.milliseconds(500), scheduler: MainScheduler.instance)
+            .subscribe(onNext: { [unowned self] input in
+                let filtered = items.filter {
+                    $0.name.hasPrefix(input)
+                }
+                filterItems.onNext(filtered)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func binding() {
         fetchItems()
-        items
-            .bind(to: tableView.rx.items(cellIdentifier: "ItemCell")) { index, element, cell in
+            .take(1)
+            .subscribe(onNext: { [weak self] in
+                self?.items = $0
+                self?.filterItems.onNext($0)
+            })
+            .disposed(by: disposeBag)
+        
+        filterItems
+            .bind(to: tableView.rx.items(cellIdentifier: "ItemCell")) { [unowned self] index, element, cell in
+                
                 cell.textLabel?.text = element.name
                 cell.detailTextLabel?.text = "\(element.money)"
-                _ = self.loadImage(from: element.avatar)
+                
+                self.loadImage(from: element.avatar)
                     .observe(on: MainScheduler.instance)
                     .subscribe(
                         onNext: { image in
@@ -42,38 +75,43 @@ class TableViewController: UIViewController {
                             
                         }
                     )
+                    .disposed(by: self.disposeBag)
             }
             .disposed(by: disposeBag)
     }
     
-    private func fetchItems() {
-        _ = Observable<[Item]>.create { emitter in
-            let url = URL(string: self.mockAPI)!
-            let task = URLSession.shared.dataTask(with: url) { (data, _, err) in
-                guard err == nil else {
-                    emitter.onError(err!)
-                    return
+    private func fetchItems() -> Observable<[Item]> {
+        return Observable<[Item]>.create { emitter in
+            weak var task: URLSessionDataTask?
+            
+            DispatchQueue.global().async { [weak self] in
+                guard let urlStr = self?.mockAPI,
+                      let url = URL(string: urlStr) else { return }
+                task = URLSession.shared.dataTask(with: url) { (data, _, err) in
+                    guard err == nil else {
+                        emitter.onError(err!)
+                        return
+                    }
+                    if let data = data,
+                       let decoded = try? JSONDecoder().decode([Item].self, from: data) {
+                        emitter.onNext(decoded)
+                    }
+                    emitter.onCompleted()
                 }
-                if let data = data,
-                   let decoded = try? JSONDecoder().decode([Item].self, from: data) {
-                    emitter.onNext(decoded)
-                }
-                emitter.onCompleted()
+                task?.resume()
             }
-            task.resume()
+            
             return Disposables.create() {
-                task.cancel()
+                task?.cancel()
             }
         }
-        .take(1)
-        .bind(to: items)
     }
     
     private func loadImage(from urlStr: String) -> Observable<UIImage?> {
         return Observable.create { emitter in
             DispatchQueue.global().async {
                 if let url = URL(string: urlStr),
-                   let data = try? Data(contentsOf: url) {
+                    let data = try? Data(contentsOf: url) {
                     emitter.onNext(UIImage(data: data))
                 } else {
                     emitter.onNext(nil)
